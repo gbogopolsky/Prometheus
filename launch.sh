89e17822fb494f4e38c2711fbdcaedc39e17dcde
torvalds42 <gbogo19@gmail.com>
2017-12-23 19:12:28 +0100

    Des modifications à launch.sh pour la clarté.

--- launch.sh
+++ launch.sh
@@ -1,10 +1,13 @@
-# Il contient des lignes de commandes ï¿½ la suite qu'il va effectuer.
+# Un fichier pour compiler et exécuter le programme
+# Il contient des lignes de commandes à la suite qu'il va effectuer.
 #
+# Pour le rendre exécutable, il suffit de taper : 
 # chmod +x launch.sh
 # 
+# Puis de l'exécuter avec : 
 # ./launch.sh
 #
-# Voilï¿½ ;)
+# Voilà ;)
 
 g++ -c Main.cpp Vecteur.cpp Objet.cpp
 g++ Main.o Objet.o Vecteur.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-systems
