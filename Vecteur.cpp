911304d883467d2b04226b0ded4a480ad57ceacf
torvalds42 <gbogo19@gmail.com>
2017-12-25 01:47:03 +0100

    Transformation en get_x et get_y. Je ne sais plus trop pourquoi...

--- Vecteur.cpp
+++ Vecteur.cpp
@@ -5,50 +5,50 @@ Vecteur::Vecteur(): x(0.) , y(0.) {};
 Vecteur::Vecteur(double x_, double y_): x(x_), y(y_) {};
 Vecteur::Vecteur(const Vecteur & vect): x(vect.x), y(vect.y) {};
 
-double Vecteur::x_ () const {
+double Vecteur::get_x() const {
   return x;
 }
 
-double Vecteur::y_ () const {
+double Vecteur::get_y() const {
   return y;
 }
 
 Vecteur & Vecteur::operator += (const Vecteur & vecteur_) {
-  x += vecteur_.x_();
-  y += vecteur_.y_();
+  x += vecteur_.get_x();
+  y += vecteur_.get_y();
   return *this;
 }
 
 Vecteur & Vecteur::operator -= (const Vecteur & vecteur_) {
-  x -= vecteur_.x_();
-  y -= vecteur_.y_();
+  x -= vecteur_.get_x();
+  y -= vecteur_.get_y();
   return *this;
 }
 
 Vecteur & Vecteur::operator = (const Vecteur & vecteur_) {
   if (&vecteur_ != this) {
-    x = vecteur_.x_();
-    y = vecteur_.y_();
+    x = vecteur_.get_x();
+    y = vecteur_.get_y();
   }
   return *this;
 }
 
 Vecteur operator + (const Vecteur & Vecteur1, const Vecteur & Vecteur2) {
-  return Vecteur(Vecteur1.x_() + Vecteur2.x_(), Vecteur1.y_() + Vecteur2.y_());
+  return Vecteur(Vecteur1.get_x() + Vecteur2.get_x(), Vecteur1.get_y() + Vecteur2.get_y());
 }
 
 Vecteur operator - (const Vecteur & Vecteur1, const Vecteur & Vecteur2) {
-  return Vecteur(Vecteur1.x_() - Vecteur2.x_(), Vecteur1.y_() - Vecteur2.y_());
+  return Vecteur(Vecteur1.get_x() - Vecteur2.get_x(), Vecteur1.get_y() - Vecteur2.get_y());
 }
 
 Vecteur operator * (const double dt_, const Vecteur & vecteur_) {
-  return Vecteur(dt_*vecteur_.x_(), dt_*vecteur_.y_());
+  return Vecteur(dt_*vecteur_.get_x(), dt_*vecteur_.get_y());
 }
 
 void affiche_x (Vecteur vecteur_) {
-  std::cout << vecteur_.x_() << std::endl;
+  std::cout << vecteur_.get_x() << std::endl;
 }
 
 void affiche_y (Vecteur vecteur_) {
-  std::cout << vecteur_.y_() << std::endl;
+  std::cout << vecteur_.get_y() << std::endl;
 }
