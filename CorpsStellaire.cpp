bfad52b1d09da7a47aefda242e5647feb9db96a4
Anqueetas <bastien.beaumont@free.fr>
2018-01-23 20:22:34 +0100

    Redirection of const_gravitationnelle in the class Objet

--- CorpsStellaire.cpp
+++ CorpsStellaire.cpp
@@ -6,14 +6,10 @@
 
 CorpsStellaire::CorpsStellaire () :
   Objet (),
-  const_gravitationnelle (9.88),
-  rayon (100.) {}
+  rayon (100.)
+{}
 
 CorpsStellaire::CorpsStellaire (const double const_gravitationnelle_, const double rayon_, const Vecteur acceleration_, const Vecteur vitesse_, const Vecteur position_, const double masse_) :
-  Objet (acceleration_, vitesse_, position_, masse_),
-  const_gravitationnelle (const_gravitationnelle_),
-  rayon (rayon_) {}
-
-double CorpsStellaire::const_gravitationnelle_ () const {
-  return const_gravitationnelle;
-}
+  Objet (const_gravitationnelle_, masse_, acceleration_, vitesse_, position_),
+  rayon (rayon_)
+{}
--- CorpsStellaire.h
+++ CorpsStellaire.h
@@ -8,10 +8,7 @@ public:
   CorpsStellaire ();
   CorpsStellaire (const double const_gravitationnelle_, const double rayon_, const Vecteur acceleration_, const Vecteur vitesse_, const Vecteur position_, const double masse_);
 
-  double const_gravitationnelle_ () const;
-
 private:
-  double const_gravitationnelle;
   double rayon;
 };
 
