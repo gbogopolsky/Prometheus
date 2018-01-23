3757e8f7a8d0a829efde836d086327cb6d0e3ce3
torvalds42 <gbogo19@gmail.com>
2018-01-23 14:05:27 +0100

    Modifying main.cpp to use new classes.

--- Main.cpp
+++ Main.cpp
@@ -6,50 +6,65 @@
 
 #include "Vecteur.h"
 #include "Objet.h"
+#include "Vaisseau.h"
+#include "CorpsStellaire.h"
 
 sf::RenderWindow window;
-sf::CircleShape Prometheus(15);
+sf::CircleShape Ship(15);
+sf::CircleShape Mars(50);
 
 int main() {
+
   //creation du système
-  Objet Vaisseau;
+  Vaisseau Prometheus(30., 10., 8., 8., Vecteur(), Vecteur(), Vecteur (), 10.);
+  CorpsStellaire Planete(9.88, 200., Vecteur(), Vecteur(), Vecteur(110,250), 300000);
+
   //création d'un fichier de sortie des données et d'un compteur d'itérations
   std::ofstream outfile;
-  outfile.open("vaisseau.dat");
-  int iterations = 0;
+
   //temps d'une boucle (fps)
   sf::Time time = sf::milliseconds(10);
-  double dTime = 0.010;
+
+  //Variable de RK4
+  double h = 0.010;
+
   // création de la fenêtre
   window.create(sf::VideoMode(800, 600), "My window");
-  Prometheus.setFillColor(sf::Color(100, 250, 50));
+
+  //Couleur des elements
+  Ship.setFillColor(sf::Color(100, 250, 50));
+  Mars.setFillColor(sf::Color(250,50,100));
+
   // on fait tourner le programme tant que la fenêtre n'a pas été fermée
   while (window.isOpen()) {
+
     //Chronometrage
     sf::Clock clock;
 
     // on traite tous les évènements de la fenêtre qui ont été générés depuis la dernière itération de la boucle
     sf::Event event;
+
     while (window.pollEvent(event)) {
+
         // fermeture de la fenêtre lorsque l'utilisateur le souhaite
         if (event.type == sf::Event::Closed)
             window.close();
     }
-    // effacement de la fenêtre en noir
+
+    //Calcul
+    Prometheus.Input();
+    Prometheus.RK4(h, Prometheus, Planete);
+
+    //Positionnement
+    Ship.setPosition(Prometheus.position_().x_(), Prometheus.position_().y_());
+    Mars.setPosition(Planete.position_().x_(), Planete.position_().y_());
+
+    //Affichage
     window.clear();
-    Vaisseau.Acceleration();
+    window.draw(Ship);
+    window.draw(Mars);
 
-    //Vaisseau.affiche_acceleration_x(Vaisseau.acceleration_x());
-    //Vaisseau.affiche_acceleration_y(Vaisseau.acceleration_y());
-    outfile << iterations << " " << Vaisseau.acceleration_x() << " " << Vaisseau.acceleration_y() << " " << Vaisseau.vitesse_x() << " "  << Vaisseau.vitesse_y() << " " << Vaisseau.x_() << " " << Vaisseau.y_() << std::endl;
-
-    Vaisseau.Euler(dTime);
-    Prometheus.setPosition(Vaisseau.x_(), Vaisseau.y_());
-    window.draw(Prometheus);
-
-    // c'est ici qu'on dessine tout
-      // fin de la frame courante, affichage de tout ce qu'on a dessiné
-    iterations += 1;
+    //Iterations
     window.display();
     sf::sleep(time - clock.getElapsedTime());
   }
