03ecb35c99b01a9af147e97889f64ecc6714b7a4
Anqueetas <bastien.beaumont@free.fr>
2018-02-12 22:25:05 +0100

    Data become a class.

---
+++ Data.cpp
@@ -0,0 +1,241 @@
+#include "Data.h"
+
+//Paramètres aléatoirs
+random_device r;
+mt19937_64  e1(r());
+
+Data::Data () :
+  n_(1),
+  t_(3),
+  R_(10.),
+  M_(0.),
+  Rmax_(120),
+  Rmin_(40),
+  Lx_(1900),
+  Ly_(1200),
+  h_(0.08),
+  Propulsion_Ar_(80),
+  Propulsion_Av_(20),
+  Propulsion_Lat_(10),
+  Propulsion_Rot_(10),
+  Masse_(1),
+  Largeur_(40),
+  Longueur_(40),
+  position_initial_(1900/2, 1200/2),
+  Ship_ (sf::Vector2f(40,40)),
+  Prometheus_(
+    80,
+    20,
+    10,
+    10,
+    Vecteur (),
+    Vecteur (),
+    Vecteur (),
+    Vecteur (1900/2, 1200/2),
+    1,
+    texture_vaisseau_,
+    Ship_
+    ),
+  list_objet_ (1),
+  corps_ (1)
+{}
+
+Data::Data (const int N,
+            const int RMAX,
+            const int RMIN,
+            const int LX,
+            const int LY,
+            const double PROPULSION_AR,
+            const double PROPULSION_AV,
+            const double PROPULSION_LAT,
+            const double PROPULSION_ROT,
+            const double MASSE
+            ) :
+  n_(N),
+  t_(3),
+  R_(10.),
+  M_(0.),
+  Rmax_(RMAX),
+  Rmin_(RMIN),
+  Lx_(LX),
+  Ly_(LY),
+  h_(0.08),
+  Propulsion_Ar_(PROPULSION_AR),
+  Propulsion_Av_(PROPULSION_AV),
+  Propulsion_Lat_(PROPULSION_LAT),
+  Propulsion_Rot_(PROPULSION_ROT),
+  Masse_(MASSE),
+  Largeur_(40),
+  Longueur_(40),
+  position_initial_(1900/2, 1200/2),
+  Ship_ (sf::Vector2f(40,40)),
+  Prometheus_(
+    PROPULSION_AR,
+    PROPULSION_AV,
+    PROPULSION_LAT,
+    PROPULSION_ROT,
+    Vecteur(),
+    Vecteur(),
+    Vecteur(),
+    Vecteur (1900/2, 1200/2),
+    MASSE,
+    texture_vaisseau_,
+    Ship_
+    ),
+  list_objet_ (N),
+  corps_ (N)
+{}
+
+int Data::n () {
+  return (n_);
+}
+
+int Data::Lx () {
+  return (Lx_);
+}
+
+int Data::Ly () {
+  return (Ly_);
+}
+
+sf::RectangleShape Data::Ship () {
+  return (Ship_);
+}
+
+Vaisseau Data::Prometheus () {
+  return (Prometheus_);
+}
+
+vector<CorpsStellaire> Data::list_objet () {
+  return (list_objet_);
+}
+
+vector<sf::CircleShape> Data::corps () {
+  return (corps_);
+}
+
+sf::Text Data::game_over () {
+  return (game_over_);
+}
+
+sf::Text Data::game_name () {
+  return (game_name_);
+}
+
+sf::Text Data::params () {
+  return (params_);
+}
+
+void Data::loading_font (string nom_font) {
+  font_.loadFromFile(nom_font + ".ttf");
+}
+
+void Data::text_GO () {
+  loading_font("contrast");
+  game_over_.setFont(font_);
+  game_over_.setString(std::string("GAME OVER"));
+  game_over_.setCharacterSize(100);
+  game_over_.setColor(sf::Color::Red);
+  game_over_.setPosition(100,100);
+}
+
+void Data::setting () {
+  loading_font("cubic");
+  game_name_.setFont(font_name_);
+  loading_font("FORCED SQUARE");
+  params_.setFont(font_txt_);
+  game_name_.setString(std::string("PROMETHEUS"));
+  game_name_.setCharacterSize(100);
+  game_name_.setColor(sf::Color::Yellow);
+  game_name_.setPosition(100, 200);
+  params_.setCharacterSize(40);
+  params_.setColor(sf::Color::Yellow);
+  params_.setPosition(100, 400);
+}
+
+void Data::corpsstellaire_generation (CorpsStellaire & CorpsStellaire_, sf::CircleShape & corps_) {
+  double x, y, vx, vy;
+  sf::Texture texture;
+  uniform_int_distribution<int> uniform_dist_R(Rmin_, Rmax_);
+  R_ = uniform_dist_R(e1);
+  uniform_int_distribution<int> uniform_dist_Lx(Rmax_, Lx_ - Rmax_);
+  uniform_int_distribution<int> uniform_dist_Ly(Rmax_, Ly_ - Rmax_);
+  uniform_int_distribution<int> uniform_dist_t(1,t_);
+  x = uniform_dist_Lx(e1);
+  y = uniform_dist_Ly(e1);
+  string m, fichier;
+  ostringstream convert;
+  int l = uniform_dist_t(e1);
+  convert << l;
+  m = convert.str();
+  fichier = "P" + m + ".png";
+  texture.loadFromFile(fichier);
+  M_ = 6e14*sqrt(R_);
+  vx = 0;
+  vy = 0;
+  Vecteur position(x,y);
+  Vecteur vitesse(vx,vy);
+  CorpsStellaire_.set_Objet(M_, vitesse, position, texture);
+  CorpsStellaire_.set_CorpsStellaire(R_, corps_);
+}
+
+void Data::mapping () {
+  for (int i = 0; i<n_; i++){
+    corpsstellaire_generation(list_objet_[i], corps_[i]);
+  };
+};
+
+bool Data::uncovering_test () {
+  bool test = true;
+  for (int j = 0; j < (n_ - 1); j++) {
+    for (int i = j+1; i < n_; i++) {
+      if (list_objet_[j].Distance(list_objet_[i].position_(), list_objet_[j].position_()) < (list_objet_[j].rayon_() + list_objet_[i].rayon_()))
+        test = false;
+    };
+  };
+  if (test == false)
+    mapping();
+  return test;
+}
+
+void Data::ship_deplacement (bool & test) {
+  Prometheus_.set_shape();
+  Prometheus_.Input_rot();
+  Prometheus_.RK4_rotation(h_);
+  Prometheus_.Input_prop();
+  Prometheus_.RK4(h_, list_objet_);
+  Prometheus_.set_texture("V1");
+  Prometheus_.assign_texture();
+  for (int i = 0; i<n_; i++) {
+    cout << list_objet_[i].position_().x_() << "                      " << list_objet_[i].position_().y_() << endl;
+    if(Prometheus_.Distance(list_objet_[i].position_(), Prometheus_.position_()) < list_objet_[i].rayon_() && test) {
+      test = false;
+    };
+  };
+}
+
+void Data::planete_deplacement (bool & test) {
+  Vecteur position_precedente(Prometheus_.position_());
+  Vecteur position_vaisseau(1900/2 - Longueur_/2 ,1200/2 - Largeur_/2);
+  Prometheus_.set_shape(position_vaisseau.x_(), position_vaisseau.y_());
+  Prometheus_.Input_rot();
+  Prometheus_.RK4_rotation(h_);
+  Prometheus_.Input_prop();
+  Prometheus_.RK4(h_, list_objet_);
+  Prometheus_.set_texture("V1");
+  Prometheus_.assign_texture();
+  for (int i =  0 ; i<n_ ; i++) {
+    //cout << list_objet[i].position_().x_() << "                      " << list_objet[i].position_().y_() << endl;
+    Vecteur position(list_objet_[i].position_() - (Prometheus_.position_() - position_precedente));
+    list_objet_[i].set_position(position);
+    //cout << list_objet[i].position_().x_() << "                      " << list_objet[i].position_().y_() << endl;
+    //cout << Prometheus.Distance(list_objet[i].position_(), Prometheus.position_()) << endl;
+    //cout << list_objet[i].rayon_() << endl;
+  };
+  for (int l = 0; l<n_; l++) {
+    if(Prometheus_.Distance(list_objet_[l].position_(), position_vaisseau) < list_objet_[l].rayon_() && test) {
+      //cout << "ECRASEMENT !!!" << endl;
+      test = false;
+    };
+  };
+}
--- Data.h
+++ Data.h
@@ -1,3 +1,5 @@
+#ifndef Data_h
+#define Data_h
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
@@ -13,54 +15,121 @@
 
 using namespace std;
 
-//Paramètrage objets
-int n = 1;
-int t = 3;
-double R = 10;
-double M = 0;
-int Rmax = 120;
-int Rmin = 40;
-int Lx = 1900;
-int Ly = 1200;
-std::vector<CorpsStellaire> list_objet(n);
-std::vector<sf::CircleShape> corps(n);
+class Data {
 
-//Paramètrage RK4
-double h = 0.08;
+public:
 
-//Paramètrage Vaisseau
-double Propulsion_Ar = 80;
-double Propulsion_Av = 20;
-double Propulsion_Lat = 10;
-double Propulsion_Rot = 10;
-double Masse = 1;
-int Largeur = 40;
-int Longueur = 40;
-Vecteur position_initial(1900/2, 1200/2);
-sf::Texture texture_vaisseau;
-sf::RectangleShape Ship(sf::Vector2f(Longueur, Largeur));
-Vaisseau Prometheus(Propulsion_Ar,
-                    Propulsion_Av,
-                    Propulsion_Lat,
-                    Propulsion_Rot,
-                    Vecteur(),
-                    Vecteur(),
-                    Vecteur(),
-                    position_initial,
-                    Masse,
-                    texture_vaisseau,
-                    Ship
-                    );
+  Data ();
+  Data (const int N,
+        const int RMAX,
+        const int RMIN,
+        const int LX,
+        const int LY,
+        const double PROPULSION_AR,
+        const double PROPULSION_AV,
+        const double PROPULSION_LAT,
+        const double PROPULSION_ROT,
+        const double MASSE
+        );
 
-//Paramètrage texte
-sf::Font font;
-sf::Font font_name;
-sf::Font font_txt;
-sf::Text game_over;
+  void loading_font (string nom_font);
+  void text_GO ();
+  void setting ();
+  void corpsstellaire_generation (CorpsStellaire & CorpsStellaire_, sf::CircleShape & corps_);
+  void mapping ();
+  bool uncovering_test ();
+  void ship_deplacement (bool & test);
+  void planete_deplacement (bool & test);
 
-sf::Text game_name;
-sf::Text params;
 
-//Paramètres aléatoirs
-random_device r;
-mt19937_64  e1(r());
+  int n ();
+  //int t () const;
+  //double R () const;
+  //double M () const;
+  //int Rmax () const;
+  //int Rmin () const;
+  int Lx ();
+  int Ly ();
+  //double h () const;
+  //double Propulsion_Ar () const;
+  //double Propulsion_Av () const;
+  //double Propulsion_Lat () const;
+  //double Propulsion_Rot () const;
+  //double Masse () const;
+  //int Largeur () const;
+  //int Longueur () const;
+  //Vecteur position_initial () const;
+  //sf::Texture texture_vaisseau () const;
+  sf::RectangleShape Ship ();
+  Vaisseau Prometheus();
+  vector<CorpsStellaire> list_objet ();
+  vector<sf::CircleShape> corps ();
+  //sf::Font font () const;
+  //sf::Font font_name () const:
+  //sf::Font font_txt () const;
+  sf::Text game_over ();
+  sf::Text game_name ();
+  sf::Text params ();
+
+
+private:
+  //Nombre d'objets
+  int n_;
+  //Nombre de texture de planètes differentes
+  int t_;
+  //Rayon d'un CorpsStellaire
+  double R_;
+  //Masse d'un CorpsStellaire
+  double M_;
+  //Rayon maximum d'un CorpsStellaire
+  int Rmax_;
+  //Rayon minimum d'un CorpsStellaire
+  int Rmin_;
+  //Taille de la fenêtre selon x
+  int Lx_;
+  //Taille de la fenêtre seon y
+  int Ly_;
+  //Paramètrage RK4
+  double h_;
+  //Propulsion arrière d'un Vaisseau
+  double Propulsion_Ar_;
+  //Propulsion avant d'un Vaisseau
+  double Propulsion_Av_;
+  //Propulsion laterale d'un Vaisseau
+  double Propulsion_Lat_;
+  //Propulsion de rotation d'un Vaisseau
+  double Propulsion_Rot_;
+  //Masse d'un Vaisseau
+  double Masse_;
+  //Largeur d'un Vaisseau
+  int Largeur_;
+  //Longueur d'un Vaisseau
+  int Longueur_;
+  //Position initial du Vaisseau
+  Vecteur position_initial_;
+  //Texture du vaisseau
+  sf::Texture texture_vaisseau_;
+  //Forme du vaisseau
+  sf::RectangleShape Ship_;
+  //vaisseau
+  Vaisseau Prometheus_;
+  //Liste de CorpsStellaire (physique)
+  vector<CorpsStellaire> list_objet_;
+  //Liste de CorpsStellaire (affichage)
+  vector<sf::CircleShape> corps_;
+  //Police game over
+  sf::Font font_;
+  //Police nom du jeu
+  sf::Font font_name_;
+  //Police texte du jeu
+  sf::Font font_txt_;
+  //Texte game over
+  sf::Text game_over_;
+  //Texte nom du jeu
+  sf::Text game_name_;
+  //Texte du jeu
+  sf::Text params_;
+
+};
+
+#endif
