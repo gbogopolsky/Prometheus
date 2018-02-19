9fe4117e177763c5bf7a3fb9b0e272fbcbf06a31
Guillaume <gbogo19@gmail.com>
2018-02-19 22:12:56 +0100

    Updating for the new structure.

--- launch.sh
+++ launch.sh
@@ -9,7 +9,7 @@
 #
 # Voil� ;)
 
-g++ -std=c++11 -c Main.cpp Vecteur.cpp Objet.cpp CorpsStellaire.cpp Vaisseau.cpp Data.cpp
-g++ -std=c++11 Main.o Objet.o Vecteur.o CorpsStellaire.o Vaisseau.o Data.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+g++ -std=c++11 -c Main.cpp Vecteur.cpp Objet.cpp CorpsStellaire.cpp Vaisseau.cpp Data.cpp Input.cpp PageSetting.cpp Text.cpp
+g++ -std=c++11 Main.o Objet.o Vecteur.o CorpsStellaire.o Vaisseau.o Data.o Input.o PageSetting.o Text.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
 #rm Main.o Objet.o Vecteur.o
 ./sfml-app
