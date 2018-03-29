ef924259f232fd0cad71c6147d5ef8e4022bf003
Guillaume <gbogo19@gmail.com>
2018-03-29 02:25:04 +0200

    Revert "Remove Input.cpp."
    This reverts commit 75c88029df3a67b44ebaec699c83197c8c6cef65.

--- launch.sh
+++ launch.sh
@@ -9,7 +9,7 @@
 #
 # Voil� ;)
 
-g++ -std=c++11 -c Main.cpp Vecteur.cpp Objet.cpp CorpsStellaire.cpp Vaisseau.cpp Data.cpp Text.cpp
+g++ -std=c++11 -c Main.cpp Vecteur.cpp Objet.cpp CorpsStellaire.cpp Vaisseau.cpp Data.cpp Input.cpp Text.cpp
 g++ -std=c++11 Main.o Objet.o Vecteur.o CorpsStellaire.o Vaisseau.o Data.o Input.o Text.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
 #rm Main.o Objet.o Vecteur.o
 ./sfml-app
