644c66f60dfac17ec85484411d8a82381530ea51
gbogopolsky <gbogo19@gmail.com>
2019-04-20 20:27:55 +0200

    Nothing to do here.

--- git-graph-log.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ruby -e "
-left  = %x(git log --graph --oneline --decorate --date-order $1 --color=always).lines
-right = %x(git log --graph --oneline --decorate --date-order $1 --pretty=format:'%C(bold green)(%cr)%C(reset) %C(bold cyan)<%an>%C(reset)').lines
-puts left.zip(right).map { |l, r| %Q(#{l.chop} #{r[/^[\s\*\\\|\/_]*(.*)$/, 1]}) }
-" | less -R
