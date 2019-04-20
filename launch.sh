b1efd848f12d1642bc48ace98e81b5187f60f2d1
gbogopolsky <gbogo19@gmail.com>
2019-04-20 20:30:01 +0200

    New script with meson and ninja.

--- launch.sh
+++ launch.sh
@@ -1,15 +1,10 @@
-# Un fichier pour compiler et ex�cuter le programme
-# Il contient des lignes de commandes � la suite qu'il va effectuer.
-#
-# Pour le rendre ex�cutable, il suffit de taper :
-# chmod +x launch.sh
-#
-# Puis de l'ex�cuter avec :
-# ./launch.sh
-#
-# Voil� ;)
+#!/bin/bash
 
-g++ -c Main.cpp Vecteur.cpp Objet.cpp
-g++ Main.o Objet.o Vecteur.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
-#rm Main.o Objet.o Vecteur.o
+# A script to meson build the project, run it and execute the visualisation Python script.
+
+meson build
+cd build
+ninja
 ./sfml-app
+cd ..
+python3 display.py
